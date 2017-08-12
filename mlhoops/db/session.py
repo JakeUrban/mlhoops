376686532b19a8a23581a257d5bf9dc9e34c9e8d
Jake Urban <jacobu@uoregon.edu>
2017-08-12 16:57:34 -0700

    Added alembic and migrations

---
+++ mlhoops/db/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declaritive import declaritive_base
+from sqlalchemy.orm import sessionmaker
+from flask import g
+
+
+def get_db():
+    engine = create_engine('mysql+pymysql://Jake@localhost/mlhoops')
+    if not hasattr(g, 'session'):
+        g.session = sessionmaker(bind=engine)()
+    return g.session
+
+
+@app.teardown_appcontext
+def close_db():
+    if hasattr(g, 'session'):
+        g.session.close()
+
+
+Base = declaritive_base()
+session = get_db()
