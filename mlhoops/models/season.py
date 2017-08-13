ebb279a7eba720f1fdbf73871227d488d44bee06
Jake Urban <jacobu@uoregon.edu>
2017-08-13 02:55:14 -0700

    Created Season model

---
+++ mlhoops/models/season.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import relationship
+
+from mlhoops.db.session import Base
+
+
+class Season(Base):
+    """
+    Represents a NCAA basketball season
+    """
+    __tablename__ = 'seasons'
+    id = Column(Integer, primary_key=True)
+    year = Column(Integer, nullable=False, unique=True)
+    tournament = relationship("Tournament", uselist=False, back_ref='season')
+
+    def __init__(self, year):
+        self.year = year
