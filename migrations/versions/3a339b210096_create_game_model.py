66617e9372c2f304dc6f4080e9afb5b0966be6dc
Jake Urban <jacobu@uoregon.edu>
2017-08-12 17:53:02 -0700

    Forgot to add files to staging

---
+++ migrations/versions/3a339b210096_create_game_model.py
@@ -0,0 +1,24 @@
+"""Create game model
+
+Revision ID: 3a339b210096
+Revises: 
+Create Date: 2017-08-12 17:12:37.113174
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3a339b210096'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
