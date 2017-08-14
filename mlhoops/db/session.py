c7f4ed08c5024f594b660ad365468e1a433742b6
Jake Urban <jacobu@uoregon.edu>
2017-08-13 18:43:18 -0700

    now grab SQLALCHEMY_DATABASE_URI from env if present

--- mlhoops/db/session.py
+++ mlhoops/db/session.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from flask import g
+from os import environment, path
 
 
 def get_db():
-    engine = create_engine('mysql+pymysql://Jake@localhost/mlhoops')
+    engine = create_engine(environment.get('SQLALCHEMY_DATABASE_URI',
+                                           'mysql+pymysql://Jake@localhost/mlhoops'))
     if not hasattr(g, 'session'):
         g.session = sessionmaker(bind=engine)()
     return g.session
