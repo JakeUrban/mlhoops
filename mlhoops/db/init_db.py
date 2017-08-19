6de872323df3251986d8ac0d8338ed0178dd6db3
Jake Urban <jacobu@uoregon.edu>
2017-08-19 01:24:48 -0700

    First draft of init_db.py

--- mlhoops/db/init_db.py
+++ mlhoops/db/init_db.py
@@ -5,6 +5,7 @@ from alembic.config import Config
 
 from mlhoops.db.session import engine, session
 from mlhoops.db.base import Base
+from mlhoops.models import Team, Player, Season, Tournament, Game
 
 
 def drop_db():
@@ -23,18 +24,52 @@ def init_db():
 def init_db_data():
     season_data = [{'year': 2016}]
     team_data = [
-        {'name': 'Oregon', 'stats_file': 'oregon.csv'},
-        {'name': 'Oregon State', 'state_file': 'oregon_state.csv'}
+        {'name': 'Oregon'},
+        {'name': 'Oregon State'}
     ]
     game_data = [
-        {'date_played': datetime.utcnow(),
-         'stats_file': 'oregon_v_oregon_state.csv', 'home_team_score': 100,
-         'away_team_score': 52, 'tournament_game': True}
+        {'date_played': datetime.utcnow(), 'tournament_game': True,
+         'home_team_score': 100, 'away_team_score': 52,
+         'home_team': team_data[0], 'away_team': team_data[1],
+         'season': season_data[0]}
     ]
     player_data = [
         {'name': 'Jordan Bell', 'team_name': 'Oregon'},
         {'name': 'Ronnie Stacy', 'team_name': 'Oregon State'}
     ]
+    for season in season_data:
+        season = Season(season['year'])
+        session().add(season)
+        session().flush()
+
+        tournament = Tournament(season.year)
+        session().add(tournament)
+        session().flush()
+
+        for team in team_data:
+            team = Team(team['name'], season.id, tournament.id)
+            session().add(team)
+            session().flush()
+
+    for player in player_data:
+        player = Player(player['name'],
+                        session().query(Team)
+                        .filter(Team.name == player['team_name']).first().id)
+        session().add(player)
+        session().flush()
+
+    for game in game_data:
+        game['home_team'] = session().query(Team).\
+            filter(Team.name == game['home_team']['name']).first()
+        game['away_team'] = session().query(Team).\
+            filter(Team.name == game['away_team']['name']).first()
+        game['season_id'] = session().query(Season).\
+            filter(Season.year == game['season']['year']).first()
+        game = Game(**game)
+        session().add(game)
+        session().flush()
+
+    session().commit()
 
 
 if __name__ == '__main__':
