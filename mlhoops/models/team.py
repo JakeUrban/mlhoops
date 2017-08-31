1ac95a4c182d286d02178b738b66c2522e1201fe
Jake Urban <jacobu@uoregon.edu>
2017-08-30 20:50:12 -0700

    added wins and losses to team model

--- mlhoops/models/team.py
+++ mlhoops/models/team.py
@@ -12,19 +12,23 @@ class Team(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(255), unique=True, nullable=False)
     season_id = Column(Integer, ForeignKey('seasons.id'), nullable=False)
+    wins = Column(Integer, nullable=False)
+    losses = Column(Integer, nullable=False)
     stats_file = Column(String(255), nullable=False)
     made_tournament = Column(Boolean, nullable=False)
     bracket = Column(String(255))
     seed = Column(Integer)
 
     def __init__(self, name, season_id, made_tournament=False, bracket=None,
-                 seed=None):
+                 seed=None, wins=0, losses=0):
         self.name = name
         self.season_id = season_id
         self.stats_file = get_team_stats_file(self)
         self.made_tournament = made_tournament
         self.bracket = bracket
         self.seed = seed
+        self.wins = wins
+        self.losses = losses
         if made_tournament and not (bracket or seed):
             exp_str = "Must specify bracket and seed with made_tournament"
             raise Exception(exp_str)
