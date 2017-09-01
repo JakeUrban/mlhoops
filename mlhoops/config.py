1442c32bbb80f72aca131f9ff77bc3d71209ae9f
Jake Urban <jacobu@uoregon.edu>
2017-08-31 22:34:39 -0700

    took out flask and tests

--- mlhoops/config.py
+++
@@ -1,3 +0,0 @@
-from flask.config import Config
-
-config = Config(root_path='..')
--- mlhoops/db/session.py
+++ mlhoops/db/session.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from flask import g
 from os import environ
 
 engine = create_engine(environ.get(
@@ -11,18 +10,6 @@ engine = create_engine(environ.get(
 
 Base = declarative_base(bind=engine)
 
-# we use this global variable in place of flask's g object when
-# we need a database connection outside of the apps context
-OUT_OF_CONTEXT_SESSION = None
-
 
 def get_db():
-    try:
-        if not hasattr(g, 'session'):
-            g.session = sessionmaker(bind=engine)()
-        return g.session
-    except RuntimeError:  # pragma: no cover
-        global OUT_OF_CONTEXT_SESSION
-        if not OUT_OF_CONTEXT_SESSION:
-            OUT_OF_CONTEXT_SESSION = sessionmaker(bind=engine)()
-        return OUT_OF_CONTEXT_SESSION
+    return sessionmaker(bind=engine)()
