7ba3345afeaac512de5bfd0dbe5254c2883245b3
Jake Urban <jacobu@uoregon.edu>
2017-09-04 10:20:15 -0700

    fixed style

--- mlhoops/util/team_util.py
+++ mlhoops/util/team_util.py
@@ -2,5 +2,6 @@ from mlhoops import config
 
 
 def team_stats_path(team):
-    dirname = '_'.join([team.name.replace(' ', '-').lower(), str(team.season_id)])
+    dirname = '_'.join([team.name.replace(' ', '-').lower(),
+                        str(team.season_id)])
     return config.PROJECT_ROOT + '/data/' + dirname
