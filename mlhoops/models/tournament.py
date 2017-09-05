eb92c3a7bd3672283a1a428d8c05446ac53a9b40
Jake Urban <jacobu@uoregon.edu>
2017-09-04 23:30:01 -0700

    got db insertions, next up, csv data

--- mlhoops/models/tournament.py
+++ mlhoops/models/tournament.py
@@ -10,8 +10,8 @@ class Tournament(Base):
     __tablename__ = 'tournaments'
     id = Column(Integer, primary_key=True)
     season_id = Column(Integer, ForeignKey('seasons.id'), nullable=False)
-    champion_id = Column(Integer, ForeignKey('teams.id'), nullable=False)
+    champion_id = Column(Integer, ForeignKey('teams.id'))
 
-    def __init__(self, season_id, champion_id):
+    def __init__(self, season_id, champion_id=None):
         self.season_id = season_id
         self.champion_id = champion_id
--- mlhoops/util/data.py
+++ mlhoops/util/data.py
@@ -1,22 +1,48 @@
 from mlhoops.scrapers import TournamentScraper, GameScraper, TeamScraper
+from mlhoops.models import Season, Tournament, Game, Team, Player
+from mlhoops.db import session
 
 
-def get_data(year):
+def get_and_insert_data(year):
     ts = TournamentScraper()
     gs = GameScraper()
     team_s = TeamScraper()
-    game_data, team_data, player_data = [], [], []
 
     games, teams, final_four, champion = ts.get_tournament_info(year)
-    for idx in range(len(games)):
-        print(games[idx])
-        endpoint = games[idx][-1]
-        games[idx] = gs.get_game_info(endpoint, games[idx][:-1])
-        game_data.append((games[idx], gs.get_game_stats(endpoint)))
+
+    season = Season(year)
+    session().add(season)
+    session().flush()
+
+    tournament = Tournament(season.id)
+    session().add(tournament)
+    session().flush()
+
     for team in teams.items():
         print(team)
         wins, losses, stats_table = team_s.get_team_info(team[1][0])
-        team_data.append((team, wins, losses, stats_table))
-        player_data.append(team_s.get_player_info(team[1][0]))
 
-    return champion, game_data, team_data, player_data
+        t = Team(team[0], season.id, made_tournament=True, bracket=team[1][2],
+                 seed=team[1][1], wins=wins, losses=losses)
+        session().add(t)
+        session().flush()
+
+        if t.name == champion:
+            print("champion: " + str(champion))
+            tournament.champion_id = t.id
+            session().flush()
+
+        player_info = team_s.get_player_info(team[1][0])
+        for player in player_info[1].items():
+            print(player[0])
+            session().add(Player(player[0], t.id))
+            session().flush()
+
+    for idx in range(len(games)):
+        g = gs.get_game_info(games[idx][-1], games[idx][:-1])
+        print(g)
+        session().add(Game(g[0], g[1], season.id, g[4], team_one_score=g[2],
+                           team_two_score=g[3], tournament_id=tournament.id))
+        session().flush()
+
+        g_stats =  gs.get_game_stats(games[idx][-1])
