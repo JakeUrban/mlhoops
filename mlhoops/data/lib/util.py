5b7aadd9edef18f7a1e3187b67f1743ba2c6b1fd
Jake Urban <jacobu@uoregon.edu>
2018-03-18 01:52:13 -0700

    Refactored data dna query functions

--- mlhoops/data/lib/util.py
+++ mlhoops/data/lib/util.py
@@ -1,4 +1,8 @@
+from mlhoops.db.queries import (team_games_before, last_year,
+                                all_games, get_teams, num_games)
 import pandas as pd
+
+from tqdm import tqdm
 
 
 def reformat_team_data(tdf, num_games):
@@ -20,3 +24,71 @@ def reformat_team_data(tdf, num_games):
             tdf = tdf.drop(columns=[feature])
 
     return tdf
+
+
+def season_upto(t, g):
+    games = team_games_before(t, g).all()
+
+    if not games:  # if this is the first game of the season
+        last_season_t = last_year(t)
+        data = last_season_t.get_data()
+        tdf = pd.DataFrame(data[1:], columns=data[0])
+        return tdf
+
+    features = set(t.features).intersection(set(g.features))
+    team_d, opp_d = {}, {}
+    for feature in features:
+        if 'Percentage' in feature:
+            continue
+        team_d[feature] = 0
+        opp_d[feature] = 0
+
+    for game in games:  # add up data from each game before g
+        t1df, t2df = game.dataframe()  # get pandas df for that game
+        team_df, opp_df = (t1df, t2df) if t.id == game.team_one else (t2df, t1df)
+        for feature in team_d:
+            team_d[feature] += sum(team_df[feature])
+            opp_d[feature] += sum(opp_df[feature])
+
+    #  Calculate percentages and averages
+    p, fg, ft, pers = '-Point ', 'Field Goal ', 'Free Throw ', 'Percentage'
+    fgs, fts, a = 'Field Goals', 'Free Throws', 'Attempts'
+    team_d[fg+pers] = team_d[fgs]/team_d[fg+a] if team_d[fg+a] else 0
+    team_d[ft+pers] = team_d[fts]/team_d[ft+a] if team_d[ft+a] else 0
+    team_d['Points Per Game'] = team_d['Points']/len(games)
+    opp_d[fg+pers] = opp_d[fgs]/opp_d[fg+a] if opp_d[fg+a] else 0
+    opp_d[ft+pers] = opp_d[fts]/opp_d[ft+a] if opp_d[ft+a] else 0
+    opp_d['Points Per Game'] = opp_d['Points']/len(games)
+    for i in ['2', '3']:
+        team_d[i+p+fg+pers] = team_d[i+p+fgs]/team_d[i+p+fg+a] if team_d[i+p+fg+a] else 0
+        opp_d[i+p+fg+pers] = opp_d[i+p+fgs]/opp_d[i+p+fg+a] if opp_d[i+p+fg+a] else 0
+
+    return pd.DataFrame([team_d, opp_d])
+
+
+def games_dataset(year, head=None):
+    games = all_games(year)
+    df = pd.DataFrame()
+    for game in tqdm(games[:head]):
+        t1, t2 = get_teams(game)
+        t1_games_before = team_games_before(t1, game).count()
+        if not t1_games_before:
+            lyt1 = last_year(t1)
+            t1_games_before = num_games(lyt1, season_id=lyt1.season_id)
+        t2_games_before = team_games_before(t2, game).count()
+        if not t2_games_before:
+            lyt2 = last_year(t2)
+            t2_games_before = num_games(lyt2, season_id=lyt2.season_id)
+        t1df = reformat_team_data(season_upto(t1, game), t1_games_before).drop(1)
+        t2df = reformat_team_data(season_upto(t2, game), t2_games_before).drop(1)
+        game_df = t1df - t2df
+        game_df.index = [game.id]
+        game_dict = game.to_dict()
+        for feature in ['team_one', 'team_two', 'team_one_score',
+                        'team_two_score', 'date_played']:
+            game_df[feature] = game_dict[feature]
+        game_df['tournament'] = 1 if game_dict['tournament_id'] else 0
+        game_df['t1_games_before'] = t1_games_before
+        game_df['t2_games_before'] = t2_games_before
+        df = df.append(game_df)
+    return df
--- mlhoops/db/queries.py
+++ mlhoops/db/queries.py
@@ -3,59 +3,43 @@ from mlhoops.models import Team, Game, Season
 
 from sqlalchemy import or_
 
-import pandas as pd
 
-
-def season_upto(t, g, num_games=False):
-    games = session().query(Game)\
+def team_games_before(t, g):
+    return session().query(Game)\
                 .filter(Game.date_played < g.date_played,
-                        or_(Game.team_one==t.id,
-                            Game.team_two==t.id)).all()  # t's games before g
+                        or_(Game.team_one == t.id,
+                            Game.team_two == t.id))
 
-    if not games:  # if this is the first game of the season
-        t_season = session().query(Season).filter(Season.id==t.season_id).first()
-        last_season_t = session().query(Team).join(Season)\
-                            .filter(Team.name==t.name,
-                                    Season.year == t_season.year-1).first()
-        data = last_season_t.get_data()
-        tdf = pd.DataFrame(data[1:], columns=data[0])
-        if num_games:
-            ng = session().query(Game).join(Season).join(Team)\
-                    .filter(Season.year==t_season.year-1,
-                            or_(Game.team_one==last_season_t.id,
-                                Game.team_two==last_season_t.id)).count()
-            return tdf, ng
-        return tdf
 
-    t_dict, opp_dict = {}, {}
-    features = set(t.features).intersection(set(g.features))  # the features shared between g and t
-    for feature in features:  # initializing t and t's opponent's dict
-        if 'Percentage' in feature:
-            continue  # not calculating percentages yet
-        t_dict[feature] = 0
-        opp_dict[feature] = 0
+def last_year(t):
+    t_season = session().query(Season).filter(Season.id == t.season_id).first()
+    last_year_t = session().query(Team).join(Season)\
+        .filter(Team.name == t.name,
+                Season.year == t_season.year-1).first()
+    return last_year_t
 
-    for game in games:  # add up data from each game before g
-        t1df, t2df = game.dataframe()  # get pandas df for that game
-        team_df, opp_df = (t1df, t2df) if t.id == game.team_one else (t2df, t1df)
-        for feature in t_dict:
-            t_dict[feature] += sum(team_df[feature])
-            opp_dict[feature] += sum(opp_df[feature])
 
-    #  Calculate percentages and averages
-    p, fg, ft, pers = '-Point ', 'Field Goal ', 'Free Throw ', 'Percentage'
-    fgs, fts, a = 'Field Goals', 'Free Throws', 'Attempts'
-    t_dict[fg+pers] = t_dict[fgs]/t_dict[fg+a] if t_dict[fg+a] else 0
-    t_dict[ft+pers] = t_dict[fts]/t_dict[ft+a] if t_dict[ft+a] else 0
-    t_dict['Points Per Game'] = t_dict['Points']/len(games)
-    opp_dict[fg+pers] = opp_dict[fgs]/opp_dict[fg+a] if opp_dict[fg+a] else 0
-    opp_dict[ft+pers] = opp_dict[fts]/opp_dict[ft+a] if opp_dict[ft+a] else 0
-    opp_dict['Points Per Game'] = opp_dict['Points']/len(games)
-    for i in ['2', '3']:
-        t_dict[i+p+fg+pers] = t_dict[i+p+fgs]/t_dict[i+p+fg+a] if t_dict[i+p+fg+a] else 0
-        opp_dict[i+p+fg+pers] = opp_dict[i+p+fgs]/opp_dict[i+p+fg+a] if opp_dict[i+p+fg+a] else 0
+def all_games(year=None):
+    q = session().query(Game)
+    if year:
+        q = q.join(Season).filter(Season.year == year)
+    return q.all()
 
-    tdf = pd.DataFrame([t_dict, opp_dict])
-    if num_games:
-        return tdf, len(games)
-    return tdf
+
+def get_teams(game):
+    t1 = session().query(Team).filter(Team.id == game.team_one).first()
+    t2 = session().query(Team).filter(Team.id == game.team_two).first()
+    return t1, t2
+
+
+def num_games(team, year=None, season_id=None):
+    if (year and season_id) or (not (year or season_id)):
+        raise ValueError("Must pass year or season_id")
+    q = session().query(Game).join(Season)\
+        .filter(or_(Game.team_one == team.id,
+                    Game.team_two == team.id))
+    if year:
+        q = q.filter(Season.year == year)
+    if season_id:
+        q = q.filter(Season.id == season_id)
+    return q.count()
